87b6a6f0bd94c9d9401cea556590a9f4b9475f53
Pius Makene <piusmajuto@gmai.com>
2024-11-05 03:37:17 +0300

    working on email receiving

--- main.py
+++ main.py
@@ -298,6 +298,7 @@ def about():
 
 MAIL_ADDRESS = os.environ.get("EMAIL_KEY")
 MAIL_APP_PW = os.environ.get("PASSWORD_KEY")
+RECEIVING_MAIL = os.environ.get("RECV_MAIL")
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
@@ -313,7 +314,7 @@ def send_email(name, email, phone, message):
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MAIL_ADDRESS, MAIL_APP_PW)
-        connection.sendmail(MAIL_ADDRESS, MAIL_APP_PW, email_message)
+        connection.sendmail(MAIL_ADDRESS, RECEIVING_MAIL, email_message)
 
 
 if __name__ == "__main__":
